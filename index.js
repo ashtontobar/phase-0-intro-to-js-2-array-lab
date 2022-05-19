30c62d36b87f4f2651f885eb7f28f3bc4a18c446
Ashton Tobar <+>
2022-05-19 17:42:53 -0500

    commit tree dd3c91551064974386dc8249c3a89fde1573f64d

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    let newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat(name) {
+    let kittyCat = ["Arnold", ...cats];
+    return kittyCat;
+}
+
+function removeLastCat() {
+    let prepCat = [...cats.slice(0,2)];
+    return prepCat;
+}
+
+function removeFirstCat() {
+    let firstCats = [...cats.slice(1)];
+    return firstCats;
+}
